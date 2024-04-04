7b17f0b4b03cd67f17c3f9b4970e2019e31d928f
kota_shiratori <swan.kouta@gmail.com>
2024-04-04 23:29:58 +0900

    🎨 コンポーネントの引数の型

--- src/components/practice.tsx
+++ src/components/practice.tsx
@@ -1,11 +1,24 @@
 import { ReactNode } from "react";
 
 //children
-const Child = ({ children }: { children: ReactNode }) => {
+const Child = ({
+  children,
+  user,
+}: {
+  children: ReactNode;
+  user: {
+    name: string;
+    age: number;
+    isChild: boolean;
+  };
+}) => {
   return (
     <div>
       <h2>Child</h2>
       {children}
+      <p>{user.name}</p>
+      <p>{user.age}歳</p>
+      <p>{user.isChild ? "子供です" : "大人です"}</p>
     </div>
   );
 };
@@ -14,7 +27,9 @@ export const Practice = () => {
   return (
     <div>
       <h1>Practice</h1>
-      <Child>親から渡った値</Child>
+      <Child user={{ name: "白鳥", age: 26, isChild: false }}>
+        親から渡った値
+      </Child>
     </div>
   );
 };
